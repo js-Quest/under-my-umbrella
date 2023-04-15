3f9caeaa469a3eaa73a72be7062206b8349b6acd
JASaddington <jasaddington@gmail.com>
2023-04-15 14:18:34 -0500

    more styling, media query, add dayjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
 var APIKey = "16d07975cebab1a166b549c8cdd345da";
 
-var timeDisplayEl = $('#currentDay');
+// function to show current time
+var timeDisplayEl = document.getElementById('time');
 function displayTime() {
-  var rightNow = dayjs().format('[Current Date & Time:] MMM DD, YYYY [ at ] hh:mm:ss a');
-  timeDisplayEl.text(rightNow);
+  var rightNow = dayjs().format('hh:mm a');
+  timeDisplayEl.textContent = rightNow;
 };
 displayTime();
 setInterval(displayTime, 1000);
+
+// function to show current date
+var dateDisplayEl = document.getElementById('date');
+function displayDate() {
+  var dateNOW = dayjs().format('dddd, MMMM D[,] YYYY');
+  dateDisplayEl.textContent = dateNOW;
+};
+displayDate();
+setInterval(displayDate, 24 * 60 * 60 * 1000);
